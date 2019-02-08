b8f5353a2217146a1298e851ff0032d87bed0fc3
Petr Chmelar <info@pchmelar.cz>
2019-02-08 17:19:49 +0100

    add create-users script

---
+++ scripts/create-users.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for i in {1..200}
+do
+  echo -ne "Creating user [$i / 200]\033[0K\r"
+  curl -s --output /dev/null -X POST -H "Content-Type: application/json" -d "{
+    \"email\": \"user${i}@gmail.com\",
+    \"pass\": \"11111111\",
+    \"firstName\": \"User${i}\",
+    \"lastName\": \"Gmail\"
+	}" "https://qest-devstack.herokuapp.com/api/auth/registration"
+done
+
+echo -ne "\033[0K\r"
+echo "Completed"
